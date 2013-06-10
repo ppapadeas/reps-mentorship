54ae8d360230434b94b4c28aff0b80ac60737625
Pierros Papadeas <ppapadeas@gmail.com>
2013-06-10 21:41:49 +0300

    Initial commit

---
+++ files/app.js
@@ -0,0 +1,108 @@
+/* Foundation v2.2.1 http://foundation.zurb.com */
+
+/* Activate the featured Orbit reel */
+$(window).load(function () {
+    $('#featured').orbit({
+        bullets: true,
+        directionalNav: false
+    });
+});
+
+jQuery(document).ready(function ($) {
+    /* Use this js doc for all application specific JS */
+    /* TABS --------------------------------- */
+    /* Remove if you don't need :) */
+
+    function activateTab($tab) {
+        var $activeTab = $tab.closest('dl').find('a.active'),
+        contentLocation = $tab.attr("href") + 'Tab';
+
+        // Strip off the current url that IE adds
+        contentLocation = contentLocation.replace(/^.+#/, '#');
+
+        //Make Tab Active
+        $activeTab.removeClass('active');
+        $tab.addClass('active');
+
+        //Show Tab Content
+        $(contentLocation).closest('.tabs-content').children('li').hide();
+        $(contentLocation).css('display', 'block');
+    }
+
+    $('dl.tabs').each(function () {
+        //Get all tabs
+        var tabs = $(this).children('dd').children('a');
+        tabs.click(function (e) {
+            activateTab($(this));
+        });
+    });
+
+    if (window.location.hash) {
+        activateTab($('a[href="' + window.location.hash + '"]'));
+        $.foundation.customForms.appendCustomMarkup();
+    }
+
+    /* PLACEHOLDER FOR FORMS ------------- */
+    /* Remove this and jquery.placeholder.min.js if you don't need :) */
+    $('input, textarea').placeholder();
+
+    /* TOOLTIPS ------------ */
+    $(this).tooltips();
+
+    /* UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE6/7/8 SUPPORT AND ARE USING .block-grids */
+    $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
+    $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
+    $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
+    $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
+
+    /* DROPDOWN NAV ------------- */
+    var lockNavBar = false;
+    $('.nav-bar a.flyout-toggle').live('click', function(e) {
+        e.preventDefault();
+        var flyout = $(this).siblings('.flyout');
+        if (!lockNavBar) {
+            $('.nav-bar .flyout').not(flyout).slideUp(500);
+            flyout.slideToggle(500, function(){
+                lockNavBar = false;
+            });
+        }
+        lockNavBar = true;
+    });
+    if (Modernizr.touch) {
+        $('.nav-bar>li.has-flyout>a.main').css({
+            'padding-right' : '75px'
+        });
+        $('.nav-bar>li.has-flyout>a.flyout-toggle').css({
+            'border-left' : '1px dashed #eee'
+        });
+    } else {
+        $('.nav-bar>li.has-flyout').hover(function() {
+            $(this).children('.flyout').show();
+        }, function() {
+            $(this).children('.flyout').hide();
+        });
+    }
+
+    /* DISABLED BUTTONS ------------- */
+    /* Gives elements with a class of 'disabled' a return: false; */
+
+    /* ALERT BOXES ------------ */
+    var clearAlert = setTimeout(function(){
+        $(".alert-box.success").fadeOut('slow');
+    }, 5000);
+
+    $(document).on("click", ".success a.close", function(event){
+        clearTimeout(clearAlert);
+    });
+
+    $(document).on("click", ".alert-box a.close", function(event) {
+        event.preventDefault();
+        $(this).closest(".alert-box").fadeOut(function(event){
+            $(this).remove();
+        });
+    });
+
+});
+
+// leaflet root
+L_ROOT_URL = '/media/leaflet/';
---
+++ files/mentorship_diagram.js
@@ -0,0 +1,141 @@
+function get_reps(users) {
+
+    var mentorship = {};
+    var original_council = [];
+
+    users.forEach(function(value, index, array) {
+        var rep = {'user_id': value.resource_uri.split('/')[4],
+                   'name': value.fullname,
+                   'url': value.profile.profile_url,
+                   'mentor': value.profile.mentor.split('/')[4]};
+
+        if (rep.mentor === rep.user_id) {
+            delete rep.mentor;
+            original_council.push(rep);
+        }
+        array[index] = rep;
+    });
+
+    users.forEach(function(mentor) {
+       var mentees = [];
+       users.forEach(function(mentee){
+           if (mentee.mentor === mentor.user_id) {
+               mentees.push(mentee);
+           }
+       });
+       if (mentees.length > 0) {
+           mentorship[mentor.user_id] = {'name': mentor.name, 'children': mentees};
+       }
+    });
+
+    return {'mentorship': mentorship, 'original_council': original_council};
+}
+
+function create_tree(tree, nodes) {
+    tree.children.forEach(function(value){
+        if (nodes[value.user_id]) {
+          value.children = nodes[value.user_id].children;
+          create_tree(value, nodes);
+        }
+    });
+
+    return tree;
+}
+
+d3.selection.prototype.moveToFront = function() {
+    return this.each(function() { this.parentNode.appendChild(this);});
+};
+
+function highlight_path(curr_node,i) {
+    var focus = d3.event.type === 'mouseover' ? true : false;
+    while (curr_node.depth>0) {
+      d3.select('#id-' + curr_node.user_id).classed('path', focus);
+      d3.select('#node-id-' + curr_node.user_id).classed('focus', focus);
+      curr_node = curr_node.parent;
+    }
+    d3.selectAll('.focus').moveToFront();
+}
+
+function create_diagram(obj) {
+
+    var diameter = 960;
+
+    var tree = d3.layout.tree()
+        .size([360, 400])
+        .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+
+    var diagonal = d3.svg.diagonal.radial()
+        .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+
+    var svg = d3.select('#mentorship-visualization-container').append('svg')
+        .attr('width', diameter)
+        .attr('height', diameter)
+        .append('g')
+        .attr('transform', 'translate(' + diameter / 2 + ',' + diameter / 2 + ')');
+
+    var nodes = tree.nodes(obj),
+        links = tree.links(nodes);
+
+    var link = svg.selectAll('.link')
+        .data(links)
+        .enter().append('path')
+        .attr('class', 'link')
+        .attr('d', diagonal)
+        .attr('id', function(d, i) { return 'id-' + d.target.user_id; });
+
+    var node = svg.selectAll('.node')
+        .data(nodes)
+        .enter().append('g')
+        .attr('class', function(d, i) {return d.children ? 'node' : 'leaf';})
+        .attr('id', function(d, i) { return 'node-id-' + d.user_id; })
+        .attr('transform', function(d) { return 'rotate(' + (d.x - 90) + ')translate(' + d.y + ')'; })
+        .on('mouseover', highlight_path)
+        .on('mouseout', highlight_path);
+
+    node.append('circle')
+        .attr('r', 4.5)
+        .attr('id', function(d, i) { return 'circle-id-' + d.user_id; });
+
+    node.append('a')
+        .attr('xlink:href', function(d) { return d.url; })
+        .append('text')
+        .attr('dy', '.31em')
+        .attr('text-anchor', function(d) { return d.x < 180 ? 'start' : 'end'; })
+        .attr('transform', function(d) { return d.x < 180 ? 'translate(8)' : 'rotate(180)translate(-8)'; })
+        .text(function(d) { return d.name; })
+        .attr('id', function(d, i) { return 'text-id-' + d.user_id; });
+
+    d3.select(self.frameElement).style('height', diameter - 150 + 'px');
+}
+
+function loader_canvas_icon_init() {
+    var sl = new CanvasLoader('profiles-loading');
+    sl.setColor('#888888');
+    sl.setDiameter(24);
+    sl.setDensity(30);
+    sl.setRange(0.8);
+    sl.setFPS(23);
+    sl.show();
+}
+
+$(document).ready(function() {
+    loader_canvas_icon_init();
+    $('#profiles-loading').append('<div>Please wait while viz magic happens!</div>');
+    $.ajax({
+             type: 'GET',
+             url: 'https://reps.mozilla.org/api/v1/rep/?limit=500',
+             async: true,
+             contentType: 'application/jsonp',
+             dataType: 'jsonp',
+
+             success: function(data) {
+               var reps = get_reps(data.objects);
+               var mentorship_tree = {};
+               mentorship_tree.name = 'Original Council';
+               mentorship_tree.children = reps.original_council;
+
+               $('#profiles-loading-wrapper').hide();
+               create_diagram(create_tree(mentorship_tree, reps.mentorship));
+             }
+         });
+});
